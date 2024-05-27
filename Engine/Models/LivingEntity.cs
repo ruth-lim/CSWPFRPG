e4afaf90ce90d4cf3d0264a056d67b4c897eead3
ruth-lim <r.limszeern@gmail.com>
2024-05-27 17:14:05 +0800

    Refactor GameItem class to be more flexible

--- Engine/Models/LivingEntity.cs
+++ Engine/Models/LivingEntity.cs
@@ -63,7 +63,7 @@ namespace Engine.Models
         public ObservableCollection<GroupedInventoryItem> GroupedInventory { get; }
 
         public List<GameItem> Weapons =>
-            Inventory.Where(i => i is Weapon).ToList();
+            Inventory.Where(i => i.Category == GameItem.ItemCategory.Weapon).ToList();
         public bool IsDead => CurrentHitPoints <= 0;
         public event EventHandler OnKilled;
         protected LivingEntity(string name, int maximumHitPoints, int currentHitPoints, int gold, int level = 1)
