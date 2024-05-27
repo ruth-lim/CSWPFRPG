16bf60721707a6ef4645834d994212d13ba15c7a
ruth-lim <r.limszeern@gmail.com>
2024-05-27 15:40:19 +0800

    Group GameItems in inventory

--- Engine/Models/Weapon.cs
+++ Engine/Models/Weapon.cs
@@ -10,7 +10,7 @@ namespace Engine.Models
     {
         public int MinimumDamage { get; set; }
         public int MaximumDamage { get; set; }
-        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) : base(itemTypeID, name, price)
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) : base(itemTypeID, name, price, true)
         {
             MinimumDamage = minDamage;
             MaximumDamage = maxDamage;
