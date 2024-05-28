187152728e7bc3f8953e1ad3b41120b6e3d47627
ruth-lim <r.limszeern@gmail.com>
2024-05-28 10:51:40 +0800

    Add function to craft items from recipe

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Engine.EventArgs;
+using Engine.Models;
 using Engine.ViewModels;
 using System.Windows;
 using System.Windows.Documents;
@@ -56,6 +57,12 @@ namespace WPFUI
             tradeScreen.Owner = this;
             tradeScreen.DataContext = _gameSession;
             tradeScreen.ShowDialog();
+        }
+
+        private void OnClick_Craft(object sender, RoutedEventArgs e)
+        {
+            Recipe recipe = ((FrameworkElement)sender).DataContext as Recipe;
+            _gameSession.CraftItemsUsing(recipe);
         }
     }
 }
